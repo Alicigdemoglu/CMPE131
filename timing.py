2b8d4942cf587b2a26f98ac13a26a48a5b41fdbe
Ali Cigdemoglu <33742088+Alicigdemoglu@users.noreply.github.com>
2021-10-15 01:30:49 -0400

    Add files via upload
    fix timing

--- timing.py
+++ timing.py
@@ -1,11 +1,10 @@
 
 import time
 # Decorator:: calculate_time
-def calculate_time():
-    begin = time.time()
-    #time.sleep(2)
-    end = time.time()
-    return("Total time " + str(end - begin))
-  
-# test code  
-#print(calculate_time())
+def calculate_time(func):
+    def wrapper():
+        start = time.time()
+        #time.sleep(2)
+        end = time.time()
+        print("Total time ",end - start)
+    return wrapper
