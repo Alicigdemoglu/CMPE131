54d289cb43921f067c70ce621c7e313abe78946b
Ali Cigdemoglu <33742088+Alicigdemoglu@users.noreply.github.com>
2021-10-15 01:35:11 -0400

    Update timing.py

--- timing.py
+++ timing.py
@@ -1,10 +1,9 @@
-
 import time
-# Decorator:: calculate_time
+# calculate the time to run the program
 def calculate_time(func):
     def wrapper():
         start = time.time()
-        #time.sleep(2)
+        time.sleep(func)
         end = time.time()
         print("Total time ",end - start)
     return wrapper
