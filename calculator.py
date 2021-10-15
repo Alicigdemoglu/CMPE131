334f83e93b301f6129f4090937947669b1ce6bae
Ali Cigdemoglu <33742088+Alicigdemoglu@users.noreply.github.com>
2021-10-14 23:56:17 -0400

    Add files via upload
    Added the four homework files

---
+++ calculator.py
@@ -0,0 +1,28 @@
+def calculator(number1,number2,operator):
+    if operator == "+":
+        #add two numbers
+        return(number1 + number2)
+    elif operator == "-":
+        #subtract two numbers
+        return(number1 - number2)
+    elif operator == "*":
+        #multiply two numbers
+        return(number1 * number2)
+    elif operator == "/":
+        #divide two numbers
+        return(number1 / number2)
+    elif operator == "//":
+        return(number1 // number2)
+    elif operator == "**":
+        return(number1 ** number2)
+    else:
+        return False
+
+# parse input fuction to validate operator and call calculation
+def parse_input():
+    n1=float(input("Enter the first number: "))
+    n2=float(input("Enter the second number: "))
+    op=input("Enter the operation: ")
+    if not (op=="+" or op=="-" or op=="*" or op=="/" or op=="//"or op=="**"):
+        return False
+    calculator(n1,n2,op)
---
+++ triple.py
@@ -0,0 +1,10 @@
+#takes in a fuction and triples it
+def tripler(func):
+    func()
+    func()
+    func()
+#test
+#def testHello():
+#    print('Hello there!')
+    
+#tripler(testHello)
